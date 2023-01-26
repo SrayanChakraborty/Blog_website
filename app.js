c18395972a079398c9001c382f228fc1785e0a4e
Srayan <srayanchakraborty.10@gmail.com>
2023-01-26 11:08:08 +0530

    Commit

--- app.js
+++ app.js
@@ -13,10 +13,12 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+const port=process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 mongoose.connect('mongodb://localhost:27017/postDb',{ useNewUrlParser: true });
+mongoose.set('strictQuery', true);
 
 const postSchema=new mongoose.Schema({
   title:String,
@@ -69,7 +71,7 @@ app.get("/posts/:postName",(req,res)=>{
 
 
 
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log("Server started on port 3000");
 });
 
