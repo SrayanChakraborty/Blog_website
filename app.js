2282f18f0f8fcbdbe711169fdbd45d8b8b7370ea
Srayan <srayanchakraborty.10@gmail.com>
2023-01-27 11:16:04 +0530

    Commit

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ const port=process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
-mongoose.connect('mongodb://localhost:27017/postDb',{ useNewUrlParser: true });
-mongoose.set('strictQuery', true);
+mongoose.connect('mongodb+srv://admin-Srayan:srayan1234@cluster0.s5xycmh.mongodb.net/postDb',{ useNewUrlParser: true });
+mongoose.set('strictQuery', false);
 
 const postSchema=new mongoose.Schema({
   title:String,
